447efedefadd9108496d82878106c849cf515294
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-15 23:25:14 +0600

    project setup done V-1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <h2>this is header</h2>
+      <div className="navbar bg-primary text-primary-content">
+        <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
+        <Link className="btn btn-ghost normal-case text-xl" to="/">
+          Home
+        </Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/login">
+          Login
+        </Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/register">
+          Register
+        </Link>
+      </div>
     </div>
   );
 };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const Main = () => {
   return (
     <div>
-      <h2>this is main</h2>
+      <Header></Header>
+
       <Outlet></Outlet>
     </div>
   );
